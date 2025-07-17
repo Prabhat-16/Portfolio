283f50ed99e7dbe87088e3dda342f7f8c0ea4123
Prabhat Mishra <mprabhat1607@gmail.com>
2025-07-17 08:37:23 +0530

    Worked on  Form

---
+++ src/components/ThankYou.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const ThankYou: React.FC = () => (
+  <div style={{ textAlign: 'center', marginTop: '4rem' }}>
+    <h2>Thank you for contacting me!</h2>
+    <p>Your message has been received. I'll get back to you soon.</p>
+  </div>
+);
+
+export default ThankYou; 
